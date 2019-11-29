72087ff16e3d900c59545bbac8b142fd2629890f
byeungchun <byeungchun.kwon@gmail.com>
2019-11-29 10:51:49 +0100

    Data reporting contract deployment script added

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 const MyStringStore = artifacts.require("MyStringStore");
+const DataReporting = artifacts.require("DataReporting");
 
 module.exports = function(deployer) {
   deployer.deploy(MyStringStore);
+  deployer.deploy(DataReporting);
 };
