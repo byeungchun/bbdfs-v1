4a12c77459c0452105548030e65b390a0d921315
byeungchun <byeungchun.kwon@gmail.com>
2019-12-04 08:27:29 +0100

    FileUpload link to DataReporting

--- client/src/components/reporting/FileUpload.js
+++ client/src/components/reporting/FileUpload.js
@@ -19,10 +19,10 @@ const FileUpload = ({ drizzle, drizzleState }) => {
 
   const setObjId2Contract = value => {
     //const { drizzle, drizzleState } = this.props;
-    const contract = drizzle.contracts.MyStringStore;
+    const contract = drizzle.contracts.DataReporting;
 
     // let drizzle know we want to call the `set` method with `value`
-    const stackId = contract.methods["set"].cacheSend(value, {
+    const stackId = contract.methods["setDataOjectId"].cacheSend(value, {
       from: drizzleState.accounts[0]
     });
   };
---
+++ readme.md
@@ -0,0 +1,7 @@
+### Addtional installation step for Windows 10
+
+- To install drizzle in Windows 10, it require specific verion of node and windows-build-tools
+
+    npm install -g --production windows-build-tools --vs2015
+    npm install node@8.16.0
+    npm install drizzle
