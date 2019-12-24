f0ef5ba8c7be9eac2c3cdf10afded5cee5230556
byeungchun.kwon <byeungchun.kwon@gmail.com>
2019-12-24 01:37:38 +0100

    test function to write message in the transaction

--- test/DataExchange.js
+++ test/DataExchange.js
@@ -34,6 +34,7 @@ contract("DataExchange", accounts => {
     });
     console.log("sig1", sig1);
     console.log("recover", web3.eth.accounts.recover(hashedVal, sig1));
+
     const privateKey =
       "0xd14ba09d3ecd148962d68623d9e4c4678087d6e5e3ed391450b009ca8e8671a7";
     const sigObj = web3.eth.accounts.sign(hashedVal, privateKey);
@@ -45,5 +46,31 @@ contract("DataExchange", accounts => {
 
   it("Contract ownder address should be created account", () => {
     assert.equal(contractOwner, accounts[0], "Who creates this contract?");
+  });
+
+  //Write message in transaction and check message is exist in the transaction
+  it("Check transaction with message", async () => {
+    let txTransfer = {};
+    await web3.eth.getAccounts().then(function(res) {
+      txTransfer.from = res[2];
+      txTransfer.to = res[3];
+    });
+    txTransfer.value = web3.utils.toWei("1");
+    txTransfer.data = web3.utils.toHex("test");
+    let transaction_hash;
+    await web3.eth.sendTransaction(txTransfer).then(function(res) {
+      console.log("transaction", res);
+      transaction_hash = res.transactionHash;
+    });
+    //console.log("transaction_hash", transaction_hash);
+    let transaction_message;
+    await web3.eth.getTransaction(transaction_hash).then(function(res) {
+      transaction_message = res.input;
+    });
+    assert.equal(
+      "test",
+      web3.utils.toUtf8(transaction_message),
+      "message is not same"
+    );
   });
 });
