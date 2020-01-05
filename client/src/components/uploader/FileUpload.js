f2f2cd1b974ab30bc21c49bef7ecff9b9b5a196f
byeungchun <byeungchun.kwon@gmail.com>
2020-01-05 17:21:51 +0100

    file upload to mongodb

--- client/src/components/uploader/FileUpload.js
+++ client/src/components/uploader/FileUpload.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from "react";
+import axios from "axios";
 import PropTypes from "prop-types";
 
 import ReceiverList from "./ReceiverList";
@@ -7,33 +8,51 @@ import ReceiverState from "../../context/receiver/ReceiverState";
 
 const FileUploader = () => {
   const [file, setFile] = useState("");
-
   const [filename, setFilename] = useState("Choose File");
 
   const onChange = e => {
     setFile(e.target.files[0]);
     setFilename(e.target.files[0].name);
   };
+  const onSubmit = async e => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
 
+    try {
+      const res = await axios.post('/api/upload', formData, {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <ReceiverState>
       <Fragment>
-        <div className="grid-row-4">
-          <div>
-            <ReceiverList />
+        <form onSubmit={onSubmit}>
+          <div className="grid-row-4">
+            <div>
+              <ReceiverList />
+            </div>
+            <div className="custom-file mb-4">
+              <input
+                type="file"
+                className="custom-file-input"
+                id="customFile"
+                onChange={onChange}
+              />
+              <input type="submit"
+                value="Upload" className="btn btn-primary btn-block mt-4" />
+              <label className="custom-file-label" htmlFor="customFile">
+                {filename}
+              </label>
+            </div>
           </div>
-          <div className="custom-file mb-4">
-            <input
-              type="file"
-              className="custom-file-input"
-              id="customFile"
-              onChange={onChange}
-            />
-            <label className="custom-file-label" htmlFor="customFile">
-              {filename}
-            </label>
-          </div>
-        </div>
+
+        </form>
       </Fragment>
     </ReceiverState>
   );
